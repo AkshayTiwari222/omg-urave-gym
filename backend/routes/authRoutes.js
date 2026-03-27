4bc005100b3ffb9b8461357305fb9b16918594cd
Akshay Tiwari <akshay.222993101@vcet.edu.in>
2026-03-27 20:48:57 +0530

    Fixed googleLogin import in authRoutes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser, loginUser } = require('../controllers/authController');
+// 👇 Add googleLogin right here inside these curly braces
+const { registerUser, loginUser, googleLogin } = require('../controllers/authController');
 
 // Map the routes to the controller functions
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-// ... your other routes
 router.post('/google', googleLogin);
 
 module.exports = router;
