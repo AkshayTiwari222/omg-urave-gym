1c992a4327dd5825eb852c284d664b8086ce8e8f
Akshay Tiwari <akshay.222993101@vcet.edu.in>
2026-03-27 20:44:01 +0530

    Fixed ReactDOM import in main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,20 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* We wrap the App with your specific Google Client ID */}
     <GoogleOAuthProvider clientId="562979913100-drf58a3lmvanu5qofu245eh17d57qqh6.apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
-  </React.StrictMode>,
-)
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
